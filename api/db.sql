50c964b2662ad1c1d6a5725f02c52d9616f5a9f8
Kevin Lin <kl1641@nyu.edu>
2016-01-17 03:40:47 -0500

    Adds functionality to the sign up form
    The owner name is removed from the database, and the sign up form is adjusted on the page to include a submit button, and a confirm password field instead of a owner's name field. The sign up form updates the database if the email is not in use and subsequently the page is reloaded.

--- api/db.sql
+++ api/db.sql
@@ -7,8 +7,7 @@ USE `suspref`;
 CREATE TABLE `suspref`.`business` (
   `bid` INT NOT NULL auto_increment,
   `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `owner_name` VARCHAR(255),
+  `email` VARCHAR(255) NOT NULL UNIQUE,
   `create_time` DATETIME NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`bid`));
